c4e9792b8d01b464b6817697355c11ccb430a379
Chris Cardle <cdcardle@gmail.com>
2018-10-08 16:15:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ function lowerCaseDrivers(drivers) {
   return drivers.map((driver) => driver.toLowerCase())
 }
 
-function nameToAttributes(drivers)
+function nameToAttributes(drivers) {
+  return drivers.map(function(driver) {
+    cosnt firstName = driver.split(" ")[0];
+    cosnt lastName = driver.split(" ")[1];
+
+    return Object.assign({}, {firstName: firstName}, {lastName: lastName})
+  })
+}
